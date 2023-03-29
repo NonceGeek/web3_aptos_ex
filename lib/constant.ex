c484e7bd8d8236d5d60cc8124c0e4a4cd1e0a50e
leeduckgo <albertschr@163.com>
2023-03-29 17:50:00 +0800

    init

--- lib/constant.ex
+++
@@ -1,7 +0,0 @@
-defmodule Web3AptosEx.Constant do
-  @moduledoc false
-
-  def endpoint(:local), do: "http://localhost:9851"
-  def endpoint(:dev), do: ""
-  def endpoint(:prod), do: ""
-end
--- test/starcoin/transaction/function_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule Web3AptosEx.Starcoin.Transaction.FunctionTest do
-  @moduledoc false
-
-  use ExUnit.Case, async: true
-
-  # doctest Web3AptosEx.Starcoin.Transaction.Function
-end
