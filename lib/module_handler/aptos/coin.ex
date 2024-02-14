0d6adceba4d2048b1ee335777032fe363d75adfb
leeduckgo <leeduckgo@gmail.com>
2024-02-14 10:39:14 +0800

    update with module_handler

--- lib/module_handler/aptos/coin.ex
+++ lib/module_handler/aptos/coin.ex
@@ -20,10 +20,17 @@ defmodule Web3AptosEx.ModuleHandler.Aptos.Coin.APT do
     end
   end
 
-  def transfer(client, acct, to, amount) do
-    {:ok, f} = ~a"0x1::coin::transfer<CoinType>(address, u64)"
-    payload = Aptos.call_function(f, ["0x1::aptos_coin::AptosCoin"], [to, amount])
+    def transfer(client, acct, to, amount) do
+    {:ok, f} = ~a"0x1::aptos_account::transfer(address, u64)"
+    payload = Aptos.call_function(f, [], [to, amount])
     Aptos.submit_txn(client, acct, payload)
   end
+  
+
+  # def transfer(client, acct, to, amount) do
+  #   {:ok, f} = ~a"0x1::coin::transfer<CoinType>(address, u64)"
+  #   payload = Aptos.call_function(f, ["0x1::aptos_coin::AptosCoin"], [to, amount])
+  #   Aptos.submit_txn(client, acct, payload)
+  # end
   
 end
