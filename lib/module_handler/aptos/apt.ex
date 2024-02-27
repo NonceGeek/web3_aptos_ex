f7f3e64b76ff5c941c1295fa3dea7fb6c0df531c
leeduckgo <leeduckgo@gmail.com>
2024-02-27 23:41:36 +0800

    update with module_handlers

---
+++ lib/module_handler/aptos/apt.ex
@@ -0,0 +1,35 @@
+defmodule Web3AptosEx.ModuleHandler.Aptos.Coin.APT do
+  @moduledoc """
+    0x1::coin
+  """
+  alias Web3AptosEx.Aptos.RPC
+  import Web3AptosEx.Aptos
+  alias Web3AptosEx.Aptos
+
+  @resources %{
+    coin_store: "0x1::coin"
+  }
+
+  def get_coin_store(client, acct) do
+    with {:ok, result} <- RPC.get_resource(
+      client,
+      acct,
+      "#{@resources.coin_store}::CoinStore<0x1::aptos_coin::AptosCoin>") do
+      result.data
+    end
+  end
+
+    def transfer(client, acct, to, amount, options \\ []) do
+    {:ok, f} = ~a"0x1::aptos_account::transfer(address, u64)"
+    payload = Aptos.call_function(f, [], [to, amount])
+    Aptos.submit_txn_with_auto_acct_updating(client, acct, payload, options)
+  end
+  
+
+  # def transfer(client, acct, to, amount) do
+  #   {:ok, f} = ~a"0x1::coin::transfer<CoinType>(address, u64)"
+  #   payload = Aptos.call_function(f, ["0x1::aptos_coin::AptosCoin"], [to, amount])
+  #   Aptos.submit_txn(client, acct, payload)
+  # end
+  
+end
--- lib/module_handler/aptos/coin.ex
+++ lib/module_handler/aptos/coin.ex
@@ -1,4 +1,4 @@
-defmodule Web3AptosEx.ModuleHandler.Aptos.Coin.APT do
+defmodule Web3AptosEx.ModuleHandler.Aptos.Coin do
   @moduledoc """
     0x1::coin
   """
@@ -10,26 +10,25 @@ defmodule Web3AptosEx.ModuleHandler.Aptos.Coin.APT do
     coin_store: "0x1::coin"
   }
 
-  def get_coin_store(client, acct) do
+  def get_coin_store(client, acct, coin_resource) do
     with {:ok, result} <- RPC.get_resource(
       client,
       acct,
-      "#{@resources.coin_store}::CoinStore<0x1::aptos_coin::AptosCoin>") do
+      "#{@resources.coin_store}::CoinStore<#{coin_resource}>") do
       result.data
     end
   end
-
-    def transfer(client, acct, to, amount, options \\ []) do
-    {:ok, f} = ~a"0x1::aptos_account::transfer(address, u64)"
-    payload = Aptos.call_function(f, [], [to, amount])
+  
+  def transfer(client, acct, to, amount, coin_resource, options \\ []) do
+    {:ok, f} = ~a"0x1::coin::transfer<CoinType>(address, u64)"
+    payload = Aptos.call_function(f, ["#{coin_resource}"], [to, amount])
     Aptos.submit_txn_with_auto_acct_updating(client, acct, payload, options)
   end
-  
 
-  # def transfer(client, acct, to, amount) do
-  #   {:ok, f} = ~a"0x1::coin::transfer<CoinType>(address, u64)"
-  #   payload = Aptos.call_function(f, ["0x1::aptos_coin::AptosCoin"], [to, amount])
-  #   Aptos.submit_txn(client, acct, payload)
-  # end
+  def register(client, acct, coin_resource, options \\ []) do
+    {:ok, f} = ~a"0x1::managed_coin::register<CoinType>()"
+    payload = Aptos.call_function(f, ["#{coin_resource}"], [])
+    Aptos.submit_txn_with_auto_acct_updating(client, acct, payload,, options)
+  end
   
 end
